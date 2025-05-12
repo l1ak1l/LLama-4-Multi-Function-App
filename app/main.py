976df608919cabc80d2a1a2a247e8559784fcd7c
l1ak1l <aceop786@gmail.com>
2025-05-12 18:27:06 +0500

    Fixed Cors issue

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.routes import chat, ocr, agents
 from app.utils.temp_cleanup import TempCleanup
+from fastapi.middleware.cors import CORSMiddleware
 import asyncio
 
 # @asynccontextmanager
@@ -13,7 +14,13 @@ import asyncio
 #     cleanup_task.cancel()
 
 app = FastAPI(title="Llama-4 API")
-
+app.add_middleware(
+CORSMiddleware,
+allow_origins=["http://localhost:3000"],  # Allows all origins
+allow_credentials=True,
+allow_methods=["*"],  # Allows all methods
+allow_headers=["*"],  # Allows all headers
+)
 # Include routers
 app.include_router(chat.router)
 app.include_router(ocr.router)
